f835fde3677b66c2b0d11204ac8487c819c2dc76
shadhinmonzur <shekh15-9027@diu.edu.bd>
2021-11-20 22:21:20 +0600

    added dot

--- src/Components/Dashboard/Payment/Payment.js
+++ src/Components/Dashboard/Payment/Payment.js
@@ -5,7 +5,7 @@ const Payment = () => {
     return (
         <div>
             <h2 className="text-center mb-4 section-title"
-        style={{ marginTop: "40px" }}>Payment method coming soon</h2>
+        style={{ marginTop: "40px" }}>Payment method coming soon..</h2>
         <div className="d-flex justify-content-center mt-5">
          
         <img className="img-fluid " src={img1} alt="" />
